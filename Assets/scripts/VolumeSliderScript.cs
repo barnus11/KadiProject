fd6d5722858f0eaf899b94bdd390ba869d561729
Márk <Mrk>
2016-11-13 18:47:03 +0100

    added background music

---
+++ Assets/scripts/VolumeSliderScript.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VolumeSliderScript : MonoBehaviour {
+	public UnityEngine.UI.Slider slider;
+	public AudioSource audio;
+	public void ChangeVolume()
+	{
+		audio.volume = slider.value;
+	}
+}
