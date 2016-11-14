6b5ac91d611f379a4d8c6c13b5cd8d9707c9081f
nNightKnight <nnightknight@gmail.com>
2016-11-14 14:49:00 +0100

    Added the start of map generation, able to read bitmap.

---
+++ Assets/Scripts/BitmapReader.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Drawing;
+
+public class BitmapReader
+{
+    private Bitmap bitmap;
+
+	public BitmapReader(String fileName)
+	{
+        bitmap = new Bitmap(fileName);
+	}
+
+    public Int16[] getBitmapSize()
+    {
+        return new Int16[] { Convert.ToInt16(bitmap.Width), Convert.ToInt16(bitmap.Height) };
+    }
+
+    public Int16[] getPixelColorAsInt(Int16 xCoordinate, Int16 yCoordinate)
+    {
+        Color pixelColor = bitmap.GetPixel(xCoordinate, yCoordinate);
+        return new Int16[] {pixelColor.R};
+    }
+}
