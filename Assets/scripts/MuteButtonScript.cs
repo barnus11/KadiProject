b556868f409181e3084854e6404ffe2bc177dfd9
3041TAN-06 <3041TAN-06@boronkay.local>
2016-11-14 16:04:18 +0100

    added background images and mute button

---
+++ Assets/scripts/MuteButtonScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MuteButtonScript : MonoBehaviour {
+    public AudioSource audio;
+    public UnityEngine.UI.Image image;
+    public UnityEngine.UI.Text text;
+    public void Click()
+    {
+        if (audio.mute)
+            audio.mute = false;
+        else
+            audio.mute = true;
+    }
+    void Update()
+    {
+        if (audio.mute)
+        {
+            image.color = new Color(255, 0, 0);
+            text.text = "Unmute";
+        }
+        else
+        {
+            image.color = new Color(0, 255, 0);
+            text.text = "Mute";
+        }
+    }
+}
