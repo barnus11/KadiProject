fab87716f4585a430011fe62f67e31a89e84ff0b
3041TAN-06 <3041TAN-06@boronkay.local>
2016-11-28 15:59:19 +0100

    added basic stat tracker to ingame scene

---
+++ Assets/scripts/IngameStatTracker.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IngameStatTracker : MonoBehaviour
+{
+    public Object healthDisplayUnit;
+    public Canvas canvas;
+    public int prevHealth = 0;
+    public int health;
+    Object[] healthDisplay;
+    void Update()
+    {
+        if (health != prevHealth)
+        {
+            for (int i = 0; i < prevHealth; i++)
+                Destroy(healthDisplay[i]);
+            healthDisplay = new Object[health];
+            for (int i = 0; i < health; i++)
+            {
+                healthDisplay[i] = (Instantiate(healthDisplayUnit, new Vector3(i * 40, 50, 0), new Quaternion(0, 0, 0, 0), canvas.transform));
+            }
+        }
+    }
+}
---
+++ Assets/scripts/SceneSwitchButtonScript.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneSwitchButtonScript : MonoBehaviour {
+    public string targetscene;
+    public void Click()
+    {
+        Application.LoadLevel(targetscene);
+    }
+}
