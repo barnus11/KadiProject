c4020209c83270f9283db6f61c7a6b9b36cee0e9
3041TAN-06 <3041TAN-06@boronkay.local>
2017-01-16 16:03:34 +0100

    Added ingame item list unit class

---
+++ Assets/scripts/ItemListElementScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ItemListElementScript : MonoBehaviour {
+    public GameObject pic;
+    public GameObject button;
+    public GameObject text;
+    public void Initialize()
+    {
+        button = new GameObject();
+        pic = new GameObject();
+        text = new GameObject();
+        button.AddComponent<UnityEngine.UI.Button>();
+        pic.transform.parent = button.transform;
+        text.transform.parent = button.transform;
+        pic.AddComponent<UnityEngine.UI.Image>();
+        text.AddComponent<UnityEngine.UI.Text>();
+    }
+}
