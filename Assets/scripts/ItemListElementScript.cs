84d8d6af465c3a98e4f527806069bf933aff72ba
3041TAN-06 <3041TAN-06@boronkay.local>
2017-02-13 15:57:20 +0100

    item list element prototype

--- Assets/scripts/ItemListElementScript.cs
+++ Assets/scripts/ItemListElementScript.cs
@@ -3,17 +3,24 @@ using System.Collections;
 
 public class ItemListElementScript : MonoBehaviour {
     public GameObject pic;
-    public GameObject button;
+    public GameObject mainobj;
     public GameObject text;
     public void Initialize()
     {
-        button = new GameObject();
+        mainobj = new GameObject();
         pic = new GameObject();
         text = new GameObject();
-        button.AddComponent<UnityEngine.UI.Button>();
-        pic.transform.parent = button.transform;
-        text.transform.parent = button.transform;
+        mainobj.AddComponent<RectTransform>();
+        pic.AddComponent<RectTransform>();
+        text.AddComponent<RectTransform>();
+        mainobj.AddComponent<UnityEngine.UI.Button>();
+        pic.GetComponent<RectTransform>().parent = mainobj.GetComponent<RectTransform>();
+        text.GetComponent<RectTransform>().parent = mainobj.GetComponent<RectTransform>();
         pic.AddComponent<UnityEngine.UI.Image>();
         text.AddComponent<UnityEngine.UI.Text>();
+        pic.GetComponent<RectTransform>().anchorMin = new Vector2(0, 0);
+        pic.GetComponent<RectTransform>().anchorMax = new Vector2(0.3f, 1);
+        text.GetComponent<RectTransform>().anchorMin = new Vector2(0.3f, 0);
+        text.GetComponent<RectTransform>().anchorMax = new Vector2(1, 1);
     }
 }
